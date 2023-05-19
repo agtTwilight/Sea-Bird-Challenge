5fb087b679f2b4f03c4a14b903119f258b87707a
agtTwilight <lukasmacmillen@gmail.com>
2023-05-19 08:27:51 -0700

    made help button redirect to new page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header>
         <p>Sea-Bird Scientific</p>
-        <a href='https://github.com/agtTwilight/Sea-Bird-Challenge#readme'><img id='help' src={question} alt='a question mark for tutorial info'></img></a>
+        <a href='https://github.com/agtTwilight/Sea-Bird-Challenge#readme' target='_blank' rel='noreferrer'><img id='help' src={question} alt='a question mark for tutorial info'></img></a>
     </header>
   )
 }
